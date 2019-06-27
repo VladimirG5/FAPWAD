a5a87d5060da4cad09f1f68e50fd0f1e649c9586
Vladimirg <vladimir.gorgieski@hotmail.com>
2019-06-27 14:39:04 +0200

    Rectangle Index Reported sheet changed

--- Fapwad/Fapwad/Classes/Obstacles/Rectangle.cs
+++ Fapwad/Fapwad/Classes/Obstacles/Rectangle.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 
 namespace Fapwad.Classes.Obstacles
 {
     public class Rectangle
     {
+        public int ID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
 
-        public Rectangle(int x, int y, int width, int height)
+        public Rectangle(int ID,int x, int y, int width, int height)
         {
+            this.ID = ID;
             this.X = x;
             this.Y = y;
             this.Width = width;
@@ -23,7 +26,24 @@ namespace Fapwad.Classes.Obstacles
 
         public void Draw(System.Drawing.Graphics g)
         {
+            Brush solid = new SolidBrush(Color.Red);
+            
+            if(ID == 0)
+            {
+                g.FillRectangle(solid, X, Y, Width, Height);
+            }
+            else if(ID == 1)
+            {
+                g.FillRectangle(solid, X-200, Y+200, Width, Height);
+            }
+            else
+            {
+                g.FillRectangle(solid, X+150, Y+150, Width, Height);
+            }
+            g.Dispose();
             // TO BE IMPLEMENTED
+
+
         }
     }
 }
--- Fapwad/Fapwad/Classes/Weapons/Index.cs
+++ Fapwad/Fapwad/Classes/Weapons/Index.cs
@@ -2,42 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
-
+using Fapwad.Classes;
 namespace Fapwad.Classes.Weapons
 {
     public class Index
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public int indexWidth { get; set; }
+        public int indexHeight { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+        //public Boolean isCollided { get; set; }
         public Index(int x, int y, int width, int height)
         {
             this.X = x;
             this.Y = y;
+            this.indexWidth = 25;
+            this.indexHeight = 50;
             this.Width = width;
             this.Height = height;
 
         }
 
-        public void Draw()
+        public void Draw(Graphics g)
         {
             // TO BE IMPLEMENTED
+            Brush solid = new SolidBrush(Color.Red);
+            g.FillRectangle(solid, X + 12, Y - 50, indexWidth, indexHeight);
+            g.Dispose();
         }
         public void Move()
         {
             // TO BE IMPLEMENTED
+            this.Y = Y - 5;
         }
-        public Boolean IsCollided()
+        public Boolean IsCollided(List<Obstacles.Rectangle> rectangles)
         {
             // TO BE IMPLEMENTED
-            throw new NotImplementedException();
+            // Checking the window
+            if (this.X < 0 || this.X > Width || this.Y < indexHeight || this.Y > Height - indexHeight)
+            {
+                return true;
+            }
+            foreach(Obstacles.Rectangle r in rectangles)
+            {
+                if (IsHit(r))
+                {
+                    return true;
+                }
+            }
+            return false;
+
         }
-        public Boolean IsHit()
+        public bool IsHit(Obstacles.Rectangle rect)
         {
-            // TO BE IMPLEMENTED
-            throw new NotImplementedException();
+            return (rect.X < this.X + this.Width) &&
+            (this.X < (rect.X + rect.Width)) &&
+            (rect.Y < this.Y + this.Height) &&
+            (this.Y < rect.Y + rect.Height);
         }
     }
 }
